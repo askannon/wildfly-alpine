eaa51560136cdf52df1fc4b1a550b307ab93358e
Thorsten Kunz <thorsten.kunz@masergy.com>
2016-05-10 08:15:04 +0000

    working version

--- 10.0/scripts/build.sh
+++ 10.0/scripts/build.sh
@@ -2,11 +2,14 @@
 
 set -ex
 
+# if we execute from a child image only run the cli commands
 if [ $# -eq 1 ] && [ "$1" = "jboss-cli-only" ]; then
   cd /opt/jboss-cli
   for configfile in `ls`; do
     $JBOSS_HOME/bin/jboss-cli.sh --file=${configfile}
   done
+  chown -R $WILDFLY_USER:$WILDFLY_GROUP $JBOSS_HOME /opt/wildfly-$WILDFLY_VERSION
+  rm -rf $JBOSS_HOME/standalone/configuration/standalone_xml_history /opt/jboss-cli/*
   exit 0
 fi
 
@@ -19,7 +22,8 @@ apk add --update \
   tcpdump \
   lsof \
   ngrep \
-  unzip
+  unzip \
+  socat
 
 # install the S6 overlay
 curl -L https://github.com/just-containers/s6-overlay/releases/download/v$S6_VERSION/s6-overlay-amd64.tar.gz | tar xz -C /
@@ -33,17 +37,19 @@ ln -s wildfly-$WILDFLY_VERSION wildfly
 # process some default configuration settings
 cd $JBOSS_HOME/standalone/configuration/
 
+# this applys to all config files
 for configfile in `ls standalone*.xml`; do
   $JBOSS_HOME/bin/jboss-cli.sh <<- _EOF_
     embed-server --server-config=${configfile}
     /subsystem=transactions/:write-attribute(name=node-identifier,value="\${jboss.node.name}")
-	/subsystem=undertow/server=default-server/host=default-host/filter-ref=server-header:remove()
-	/subsystem=undertow/server=default-server/host=default-host/filter-ref=x-powered-by-header:remove()
-	/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)
-	/subsystem=undertow/server=default-server/http-listener=default:undefine-attribute(name=redirect-socket)
+    /subsystem=undertow/server=default-server/host=default-host/filter-ref=server-header:remove()
+    /subsystem=undertow/server=default-server/host=default-host/filter-ref=x-powered-by-header:remove()
+    /subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)
+    /subsystem=undertow/server=default-server/http-listener=default:undefine-attribute(name=redirect-socket)
 _EOF_
 done
 
+# this only applies to HA config files
 for configfile in `ls standalone*ha.xml`; do
   $JBOSS_HOME/bin/jboss-cli.sh <<- _EOF_
     embed-server --server-config=${configfile}
@@ -64,8 +70,8 @@ for configfile in `ls standalone*ha.xml`; do
     /subsystem=jgroups/stack=tunnel/transport=TRANSPORT/property=gossip_router_hosts:add(value="\${jgroups.gossip_router_hosts}")
     /subsystem=jgroups/channel=ee:write-attribute(name=stack,value=tunnel)
     /subsystem=logging/logger=org.jgroups/:add(category=org.jgroups,level=DEBUG)
-	/subsystem=infinispan/cache-container=web/distributed-cache=dist/transaction=TRANSACTION:add(locking=OPTIMISTIC)
-	/subsystem=infinispan/cache-container=web/distributed-cache=dist/locking=LOCKING:add(isolation=READ_COMMITTED)
+    /subsystem=infinispan/cache-container=web/distributed-cache=dist/transaction=TRANSACTION:add(locking=OPTIMISTIC)
+    /subsystem=infinispan/cache-container=web/distributed-cache=dist/locking=LOCKING:add(isolation=READ_COMMITTED)
     run-batch
 _EOF_
 done
@@ -74,10 +80,10 @@ done
 curl -L http://central.maven.org/maven2/org/jolokia/jolokia-war/$JOLOKIA_VERSION/jolokia-war-$JOLOKIA_VERSION.war -o /tmp/jolokia.war
 unzip /tmp/jolokia.war -d $JBOSS_HOME/standalone/deployments/jolokia.war
 
-# manage run as user/group
+# add run as user/group
 addgroup -g $WILDFLY_GID $WILDFLY_GROUP
 adduser -D -G $WILDFLY_GROUP -s /bin/false -u $WILDFLY_UID $WILDFLY_USER
 chown -R $WILDFLY_USER:$WILDFLY_GROUP $JBOSS_HOME /opt/wildfly-$WILDFLY_VERSION
 
 # cleanup
-rm -rf /tmp/* /var/cache/apk/*
+rm -rf /tmp/* /var/cache/apk/* $JBOSS_HOME/standalone/configuration/standalone_xml_history
--- 10.0/scripts/run.sh
+++ 10.0/scripts/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "OS Environment:"
+echo "Process Environment:"
 echo "========================================================================="
 env | sort
 echo "========================================================================="
 
-# add potential statup delays due to broken /deb/random
+# eliminate potential statup delays due to broken /dev/random
 ADDL_JAVA_OPTS="$ADDL_JAVA_OPTS -Djava.security.egd=file:/dev/./urandom";
 
 if [[ ${CONFIG_FILE} ]] && [[ ${CONFIG_FILE} == *ha.xml ]]; then
@@ -18,10 +18,24 @@ if [[ ${CONFIG_FILE} ]] && [[ ${CONFIG_FILE} == *ha.xml ]]; then
   fi
 fi
 
-if [[ $JOLOKIA_OFF ]]; then
-  touch $JBOSS_HOME/standalone/deployments/jolokia.war.skipdeploy
-else
+if [[ $JOLOKIA_ENABLED == "true" ]]; then
+  echo "Jolokia enabled!"
   touch $JBOSS_HOME/standalone/deployments/jolokia.war.dodeploy
+else
+  touch $JBOSS_HOME/standalone/deployments/jolokia.war.skipdeploy
 fi
 
-exec s6-applyuidgid -u 1000 -g 1000 $JBOSS_HOME/bin/standalone.sh -Djboss.node.name=${NODE_NAME:-$HOSTNAME} -b $(hostname -i) -bmanagement $(hostname -i) -c ${CONFIG_FILE:-standalone.xml} $ADDL_JAVA_OPTS
+if [ "x$JBOSS_MODULES_SYSTEM_PKGS" = "x" ]; then
+   JBOSS_MODULES_SYSTEM_PKGS="org.jboss.byteman"
+fi
+
+if [ "x$JAVA_OPTS" = "x" ]; then
+   JAVA_OPTS="-Xms64m -Xmx768m -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
+   JAVA_OPTS="$JAVA_OPTS -Djboss.modules.system.pkgs=$JBOSS_MODULES_SYSTEM_PKGS -Djava.awt.headless=true"
+else
+   echo "JAVA_OPTS already set in environment; overriding default settings with values: $JAVA_OPTS"
+fi
+
+export JAVA_OPTS="$JAVA_OPTS $ADDL_JAVA_OPTS"
+
+exec s6-applyuidgid -u 1000 -g 1000 $JBOSS_HOME/bin/standalone.sh -Djboss.node.name=${NODE_NAME:-$HOSTNAME} -b $(hostname -i) -bmanagement $(hostname -i) -c ${CONFIG_FILE:-standalone.xml}
